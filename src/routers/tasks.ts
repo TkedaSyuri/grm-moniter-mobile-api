a2c006607fe30d32bfa57ff2d162e2b28e8b239f
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-04-21 16:34:53 +0900

    task api作成

--- src/routers/tasks.ts
+++ src/routers/tasks.ts
@@ -2,15 +2,30 @@
 import { Hono } from "hono";
 import { db } from '../db/database'
 import { task } from '../drizzle/schema'
+import { eq } from "drizzle-orm";
 
 const app = new Hono()
 
 app.get('/', async (c) => {
+  try{
     const allTask = await db.select().from(task)  
-    return c.json(allTask)
+    return c.json(allTask,200)
+  }catch(e){
+    return c.json({message:"データの取得に失敗"},200)
+  }
   })
-  
 
+
+app.put("/complete-task/:id",async (c)=>{
+  const id = parseInt(c.req.param("id"));
+  try{
+    await db.update(task).set({isCompleted:true}).where(eq(task.id,id))
+    return c.json({message:"データの更新に成功"},200)
+  }catch(e){
+    return c.json({message:"データの更新に失敗"},200)
+
+  }
+})
 
 
 export default app;
