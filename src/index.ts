613025fc1204b343e4154227a62b91fb1bc77bee
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-09-06 16:03:33 +0900

    corsの設定を削除

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
-import { cors } from 'hono/cors'
 import { Server as SocketServer } from 'socket.io'
 
 import roomRoute from './routers/rooms'
@@ -8,7 +7,6 @@ import taskRoute from './routers/tasks'
 import chatRoute from './routers/chats'
 
 const app = new Hono()
-app.use(cors())
 
 const PORT = Number(process.env.PORT) || 10000
 
@@ -18,12 +16,7 @@ const httpServer = serve({
 })
 
 // socket.io を Honoサーバーにアタッチ
-export const io = new SocketServer(httpServer as any, {
-  cors: {
-    origin: "*", 
-    methods: ["GET", "POST"],
-  },
-})
+export const io = new SocketServer(httpServer as any)
 
 app.route('/api/room', roomRoute)
 app.route('/api/task', taskRoute)
